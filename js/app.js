5b43544a50146cbe8e667126ca04b069d0a41f9f
Fabio Ribeiro <fabio@Fabios-MacBook.local>
2015-10-04 19:24:11 -0400

    weather icon, layout, color logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
-var currTemp = 82	 // To be fed by API
+var currTemp = Math.random() * (70) + 30;	 // To be fed by API
 var normalTemp = 82; // To be fed by normal data downloaded and parsed for location
 var oMessages;
 var chosenMessage;
@@ -67,28 +67,28 @@ function DoMsgLogic() {
 			// Case 5 - for really fucking hot
 			chosenMessage = PickOneMessage(5);
 			console.log("Case5");
-			$(".message-block").css( "background", "red" );  
+			$("body").css( "background", "red" );  
 			
 		} else if (currTemp < .9 * normalTemp) {
 
 			// Case 1 - for really fucking cold 
 			chosenMessage = PickOneMessage(1);
 			console.log("Case1");
-			$(".message-block").css( "background", "blue" );  
+			$("body").css( "background", "blue" );  
 
 		} else if (currTemp >= normalTemp) { 
 
 			// Case 3 - for hot as usual
 			chosenMessage = PickOneMessage(3);
 			console.log("Case3");
-			$(".message-block").css( "background", "red" );  
+			$("body").css( "background", "red" );  
 
 		} else if (currTemp < normalTemp) {
 
 			// Case 2 - for cold as usual
 			chosenMessage = PickOneMessage(2);
 			console.log("Case2");
-			$(".message-block").css( "background", "blue" );  
+			$("body").css( "background", "blue" );  
 
 		}
 
